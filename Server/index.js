a3145549ea8a7430fb952925c304ae8d67251d1d
Dishancp <it20012106@my.sliit.lk>
2021-09-13 01:48:54 +0530

    Changed Folder Structure and Add Backend

---
+++ Server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require('cors');
+
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+const connectDB = require("./src/config/config");
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Hello Node!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
---
+++ Server/src/config/config.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const URI = "mongodb://localhost:27017/teaApp?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
+const connectDB = async () => {
+    await mongoose.connect(URI);
+    console.log("Database Connected");
+}
+
+
+module.exports = connectDB;
