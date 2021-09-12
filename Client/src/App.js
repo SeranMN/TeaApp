85b1c12e8e646ba23c967db72c21ab99c231e694
SeranMN <navindu263@gmail.com>
2021-09-12 20:32:05 +0530

    Changed folder structure

---
+++ Client/src/App.js
@@ -0,0 +1,16 @@
+import Header from './component/Header';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './component/Footer';
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <h1>Gangasiri</h1>
+     <Footer/>
+     
+    </div>
+  );
+}
+
+export default App;
